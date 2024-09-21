f3ac418d169e694dfc3545bbd16ccac86cf6e0f7
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2024-09-21 22:39:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function stringChop(str, size) {
   // your code here
-	if(!str) return[];
+	if(!str || size  <= 0) return[];
 	let chunks = [];
-		for(let i =0; i< chunks.size;i+=size){
-			let subStr = str.slice(i, i+size);
-			chunks.push(subStr);
-		}
-	return chunks;
-	
+		for (let i = 0; i < str.length; i += size) {
+    let subStr = str.slice(i, i + size);
+    chunks.push(subStr);
+  }
+  
+  return chunks;
 }
 
 // Do not change the code below
